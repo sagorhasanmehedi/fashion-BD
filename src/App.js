c3f169223f9596f6b91b99b43dc447c775f4976d
sagorhasanmehedi <mehedihasansagor1995@gmail.com>
2022-10-27 13:01:29 +0600

    add header

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import './App.css';
+import "./App.css";
+import Home from "./pages/Home";
 
 function App() {
   return (
     <div className="App">
-     <h1>OKKKKK</h1>
+      <Home />
+      <h1>OKKKKK</h1>
     </div>
   );
 }
---
+++ src/components/home/Header.js
@@ -0,0 +1,21 @@
+import React from "react";
+import Stack from "@mui/material/Stack";
+import Button from "@mui/material/Button";
+
+const Header = () => {
+  return (
+    <div>
+      <Stack direction="row" spacing={2}>
+        <Button color="secondary">Secondary</Button>
+        <Button variant="contained" color="success">
+          Success
+        </Button>
+        <Button variant="outlined" color="error">
+          Error
+        </Button>
+      </Stack>
+    </div>
+  );
+};
+
+export default Header;
